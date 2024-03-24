a8de88bc348373dd575ae87cb614c1a307b5bc0b
Based-Anaxes <anaxes808@gmail.com>
2024-03-24 12:51:04 -0400

    Add event listener to numBtns
    This changes 'display.textContent' depending on which button the user
    clicked.

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
+const display = document.querySelector('.display')
 const numBtns = document.querySelectorAll('.number');
+
+numBtns.forEach((numBtn) => {
+    numBtn.addEventListener('click', () => {
+        switch(numBtn.id) {
+            case "seven": 
+                display.textContent = "7";
+                break; 
+            case "eight":
+                display.textContent = "8";
+                break; 
+            case "nine": 
+                display.textContent = "9";
+                break;
+            case "four": 
+                display.textContent = "4";
+                break; 
+            case "five":
+                display.textContent = "5";
+                break; 
+            case "six": 
+                display.textContent = "6";
+                break;
+                case "three": 
+                display.textContent = "3";
+                break; 
+            case "two":
+                display.textContent = "2";
+                break; 
+            case "one": 
+                display.textContent = "1";
+                break;
+            case "zero": 
+                display.textContent = "0";
+                break;
+        }
+        console.log(display.textContent);
+    })
+})
 
 const operations = {
     '+': function(num1, num2) {
