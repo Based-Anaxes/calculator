01fcc632976102f53589ca84beaa758e915f17d3
Based-Anaxes <anaxes808@gmail.com>
2024-03-25 16:41:07 -0400

    Convert num to an integer
    The digits were stored in an array and then reduced to a single string
    value. This meant that users would not have been able to perform
    operations on any of their inputs. Converting this string value into a
    number fixes this issue.

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
+// Create an integer variable for num1
+let num1 = 0; 
+// Create an integer variable for num2
+let num2 = 0; 
+// Create an object for operators
+const operators = {};
+// Create an array variable for digits
 let digits = [];
-const display = document.querySelector('.display')
+// Create a node for display
+const display = document.querySelector('.display');
+//Create a string variable for displayValue
+let displayValue = '';
+// Create a nodelist for numBtns 
 const numBtns = document.querySelectorAll('.number');
-const operations = {
-    '+': function(num1, num2) {
-        return num1 + num2;
-    },
-    '-': function(num1, num2) {
-        return num1 - num2;
-    },
-    '*': function(num1, num2) {
-        return num1 * num2;
-    },
-    '/': function(num1, num2) {
-        return num1 / num2;
-    }
-};
-
+//For each numBtn that a user clicks store the numBtn's value in digits
 numBtns.forEach((numBtn) => {
-    numBtn.addEventListener('click', () => {
-        digits.push(numBtn.value);
-        let num = digits.reduce((digit, current) => digit + current);
-        display.textContent = num;
-    })
-})
+    numBtn.addEventListener('click', () => getNum(numBtn));
+});
 
-function operate(operator, num1, num2) {
-    return operations[operator](num1, num2);
+function getNum(numBtn) {
+    digits.push(numBtn.value);
+    //Convert digits to a string and store in num
+    let num = digits.reduce((value, currentDigit) => value + currentDigit);
+    //Store num in displayValue
+    displayValue = num; 
+    //Store displayValue in display,textContent
+    display.textContent = displayValue;
+    //Convert num to an integer and store in num1
+    num1 = Number(num);
+    console.log(num1);
 }
+ 
