97e558037e042c360a4ffb7e3d723010cd2bc371
Based-Anaxes <anaxes808@gmail.com>
2024-04-02 15:39:12 -0400

    Refactor event handler for userInput
    The event handler for userInput was very messy and, as a result,
    difficult to maintain. This proved especially tricky for updating the
    value of num2 and displaying it in a clean way.
    
    The refactored code handles number button clicks and operator button
    clicks seperately. This helps in updating num2 for number button clicks
    after an operator button has been clicked.

--- script.js
+++ script.js
@@ -1,40 +1,60 @@
-//Create constants for display element and user input
 const display = document.querySelector('.display');
-const userInput = document.querySelector('.buttons');
-//Initialize display text
-let displayText = '0';
-//Create an emoty array variable for digits 
-let digits = [];
-//Create an empty string variable for operator
-let operator = '';
-//Create a variable digit 
-let digit; 
-//Create function updateDisplay that takes digit and operator as parameters
-function updateDisplay(digit, operator) {
-	//Push digit to digits
-    digits.push(digit);
-	//If digits' length is less than ten
-    if(digits.length < 10) {
-		//Convert digits to a string a store in displayText
-        displayText = digits.reduce((num, current) => num + current);
-    }
-	//Update display's text content with displayText
-    display.textContent = displayText + operator;
+const userInput = document.querySelector('.userInput');
+const operators = {
+	'+': function(num1, num2) {
+		return num1 + num2; 
+	},
+	'-': function(num1, num2) {
+		return num1 - num2;
+	}, 
+	'*': function(num1, num2) {
+		return num1 * num2
+	}, 
+	'/': function(num1, num2) {
+		return num1 / num2; 
+	}
 }
-//When the user clicks user input
+
+let num1 = 0;
+let num2 = 0; 
+let operator = ''
+let displayContent = '0';
+
+function handleOperatorInput(btnValue) {
+	operator = btnValue;
+	num1 = Number(displayContent);
+	displayContent = '0';
+	display.textContent = num1 + operator;
+}
+
+function handleNumberInput(btnValue) {
+	if(displayContent.length < 9) {
+		displayContent += btnValue;
+	}
+	if(displayContent[0] === '0') {
+		displayContent = displayContent.substring(1);
+	}
+	if(operator !== '') {
+		num2 = Number(displayContent);
+	}
+	console.log(num2);
+	display.textContent = displayContent;
+}
+
+function operate(operator, num1, num2) {
+	let result = operators[operator](num1, num2);
+	console.log(result);
+}
+
 userInput.addEventListener('click', (event) => {
-    //Extract the value and class of the clicked button
-    let value = event.target.value;
-    let type = event.target.className;
-	//If a number button was clicked
-    if(type === 'number') {
-		//Store value in digit
-        digit = value;
-    //If an operator buttton was clicked 
-    } else if(type === 'operator') {
-		//Store value in operator 
-        operator = value; 
-    }
-	//Call updateDisplay
-    updateDisplay(digit, operator);
+	let btnValue = event.target.value; 
+	let btnClass = event.target.className;
+	switch(btnClass) {
+		case 'number':
+			handleNumberInput(btnValue); 
+			break;
+		case 'operator':
+			handleOperatorInput(btnValue);
+			break;
+	}
 });
